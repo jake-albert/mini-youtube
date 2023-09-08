6ff148f8c5d7e8b6486e20b9b6bef7c840dcb904
Jake Albert <jake@wisefour.com>
2023-09-08 20:38:32 +0900

    introduce local video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import ffmeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing input or output video path")
+  }
+
+  ffmeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") //360p
+    .on("end", () => {
+      return res.status(200).send("Processing finished successfully.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`)
+    })
+    .save(outputFilePath)
+
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`)
-})
+});
